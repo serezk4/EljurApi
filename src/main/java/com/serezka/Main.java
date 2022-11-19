44f88ed300c52d7b62173b884e751ce1fd67c039
serezk4 <serezadorohin@gmail.com>
2022-11-20 01:48:24 +0700

    update

--- src/main/java/com/serezka/Main.java
+++ src/main/java/com/serezka/Main.java
@@ -1,12 +1,12 @@
 package com.serezka;
 
 import com.serezka.eljur.EljurApi;
-import lombok.extern.log4j.Log4j;
 import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 @Log4j2
 public class Main {
@@ -18,15 +18,27 @@ public class Main {
         EljurApi eljurApi = new EljurApi(eljurDevKey, eljurVendor, eljurAuthKey);
 
         Calendar fromDate = GregorianCalendar.getInstance();
-        fromDate.add(Calendar.DAY_OF_MONTH, -7);
+        fromDate.add(Calendar.DAY_OF_MONTH, -14);
 
         Calendar toDate = GregorianCalendar.getInstance();
+
+        System.out.println(eljurApi.requestMarks(fromDate, toDate).keySet().toString());
 
         System.out.println(eljurApi.requestMarks(fromDate, toDate)
                 .values()
                 .iterator().next()
-                .values().iterator().next()
-                .get(0).getMtype());
+                .values().iterator().next().stream().map(EljurApi.Marks.Mark::getValue).toList());
+
+        System.out.println();
+
+        eljurApi.requestHomework("11Т", fromDate, toDate).values().stream().map(EljurApi.Hometasks.Day::getItems)
+                .forEach(items -> {
+                    items.stream().map(EljurApi.Hometasks.Item::getHomework).forEach(hw -> {
+                       hw.values().stream().map(EljurApi.Hometasks.Homework::getValue).forEach(System.out::println);
+                    });
+                });
+
+        System.out.println(eljurApi.requestAuthorizationToken("\\login//", "\\pass//").token);
 
 
 
--- src/main/java/com/serezka/eljur/EljurApi.java
+++ src/main/java/com/serezka/eljur/EljurApi.java
@@ -1,6 +1,7 @@
 package com.serezka.eljur;
 
 import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.log4j.Log4j2;
@@ -25,6 +26,8 @@ public class EljurApi {
 
     String mainUrl;                     // eljur requests main url
 
+    private static final Gson gson = new Gson();
+
     public EljurApi(String eljurDevKey, String eljurVendor, String eljurAuthorizationToken) {
         this.eljurDevKey = eljurDevKey;
         this.eljurVendor = eljurVendor;
@@ -45,51 +48,39 @@ public class EljurApi {
     // get marks method
 
     /**
-     * @param fromDate - | between witch dates return marks
-     * @param toDate -   |
-     * @return marks for selected dates
-     * @throws IOException - when connect to server may throw error
+     * @param fromDate - | between witch \
+     * @param toDate -   |                \ dates return marks
+     * @return success -> marks for selected dates in format Map<[student name], Map<[lesson]>, List<[marks]>>> | error -> empty map
      */
-    public Map<String, Map<MarksRequest.Lesson, List<MarksRequest.Mark>>> requestMarks(Calendar fromDate, Calendar toDate) throws IOException {
+    public Map<String, Map<Marks.Lesson, List<Marks.Mark>>> requestMarks(Calendar fromDate, Calendar toDate)  {
         // generate link
         String requestMarksUrl = generateLink(
                 "getmarks",
-                Map.of("days", API_DATE_FORMAT.format(fromDate.getTime()) + "-" + API_DATE_FORMAT.format(toDate.getTime())));
+                Map.of("days", API_DATE_FORMAT.format(fromDate.getTimeInMillis()) + "-" + API_DATE_FORMAT.format(toDate.getTimeInMillis())));
 
         // parse data
-        Gson gson = new Gson();
+        URL     apiRequest  ;
+        String  apiResponse ;
 
-        // get data from url
-        URL url = new URL(requestMarksUrl);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-
-        int responseCode = connection.getResponseCode();
-        if (responseCode != HttpURLConnection.HTTP_OK) {
-            log.warn("response code: {}", responseCode);
+        try {
+            apiRequest      = new URL(requestMarksUrl);
+            apiResponse  = ApiUtils.parseUrl(apiRequest);
+        } catch (IOException e) {
+            log.warn(e.getMessage());
             return Collections.emptyMap();
         }
 
-        // read data
-        StringBuilder responseText = new StringBuilder();
-        InputStream inputStream = connection.getInputStream();
-
-        byte[] buffer = new byte[1024];
-        for (int temp; (temp = inputStream.read(buffer)) != -1; )
-            responseText.append(new String(buffer, 0, temp));
-
-        // deserialize to object
-        MarksRequest.Response response = gson.fromJson(responseText.toString(), MarksRequest.Root.class).response;
+        // get data from url and deserialize to object
+        Marks.Response response = gson.fromJson(apiResponse, Marks.Root.class).response;
         if (response.error != null) {
             log.warn("response error: {}", response.error);
             return Collections.emptyMap();
         }
 
         // format
-        Map<String, Map<MarksRequest.Lesson, List<MarksRequest.Mark>>> marks = new HashMap<>();
+        Map<String, Map<Marks.Lesson, List<Marks.Mark>>> marks = new HashMap<>();
         response.result.students.forEach((s, student) -> {
-            Map<MarksRequest.Lesson, List<MarksRequest.Mark>> tempStudentMarks = new HashMap<>();
+            Map<Marks.Lesson, List<Marks.Mark>> tempStudentMarks = new HashMap<>();
             student.lessons.forEach(lesson -> tempStudentMarks.put(lesson, lesson.marks));
             marks.put(student.name, tempStudentMarks);
         });
@@ -97,7 +88,100 @@ public class EljurApi {
         return marks;
     }
 
-    public static class MarksRequest {
+    // get homework request
+
+    /**
+     * @param schoolClass - selected school class name
+     * @param fromDate - | between witch \
+     * @param toDate   - |                \ dates return marks
+     * @return success -> homework for selected dates in format Map<[day], [day data]> | error -> empty map
+     */
+    public Map<String, Hometasks.Day> requestHomework(String schoolClass, Calendar fromDate, Calendar toDate) {
+        String requestHomeworkUrl = generateLink("gethomework", Map.of("class", schoolClass, "days",
+                API_DATE_FORMAT.format(fromDate.getTime()) + "-" + API_DATE_FORMAT.format(toDate.getTime())));
+
+        // get data from url
+        URL apiRequest;
+        String apiResponse;
+
+        try {
+            apiRequest = new URL(requestHomeworkUrl);
+            apiResponse = ApiUtils.parseUrl(apiRequest);
+        } catch (IOException e) {
+            log.warn(e.getMessage());
+            return Collections.emptyMap();
+        }
+
+        // deserialize to object
+        Hometasks.Response response = gson.fromJson(apiResponse, Hometasks.Root.class).response;
+        if (response.error != null) {
+            log.warn("response error: " + response.error);
+            return Collections.emptyMap();
+        }
+
+        return response.result.days;
+    }
+
+    /**
+     * @param eljurLogin    - eljur account username
+     * @param eljurPassword - eljur account password
+     * @return success -> authorization token data | error -> null
+     */
+    public Authorization.TokenInfo requestAuthorizationToken(String eljurLogin, String eljurPassword) {
+        String requestAuthorizationTokenUrl = generateLink("auth", Map.of("login", eljurLogin, "password", eljurPassword));
+
+        // get data from url
+        URL apiRequest;
+        String apiResponse;
+
+        try {
+            apiRequest = new URL(requestAuthorizationTokenUrl);
+            apiResponse = ApiUtils.parseUrl(apiRequest);
+        } catch (IOException e) {
+            log.warn(e.getMessage());
+            return null;
+        }
+
+        // deserialize to object
+        Authorization.Response response = gson.fromJson(apiResponse, Authorization.Root.class).response;
+        if (response.error != null) {
+            log.warn(response.error);
+            return null;
+        }
+
+        return response.tokenInfo;
+    }
+
+    // TODO: 20.11.2022
+    // можно добавить методы для каждого пункта из официально api: [https://eljur.ru/api/]
+    // TODO: 20.11.2022
+
+    // utils
+    private static class ApiUtils {
+        private static String parseUrl(URL url) throws IOException {
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                log.warn("response code: {}", responseCode);
+                return null;
+            }
+
+            StringBuilder responseText = new StringBuilder();
+            InputStream inputStream = connection.getInputStream();
+
+            byte[] buffer = new byte[1024];
+            for (int temp; (temp = inputStream.read(buffer)) != -1; )
+                responseText.append(new String(buffer, 0, temp));
+
+            return responseText.toString();
+        }
+    }
+
+    // classes to parse json
+    public static class Marks {
         @FieldDefaults(level = AccessLevel.PRIVATE)
         @Getter
         public static class Root {
@@ -156,11 +240,86 @@ public class EljurApi {
         }
     }
 
-    // get homework request
-    public Object requestHomework() {
+    public static class Hometasks {
+        // formatted
 
+        @FieldDefaults(level = AccessLevel.PRIVATE)
+        @Getter
+        public static class Root {
+             Response response;
+        }
 
+        @FieldDefaults(level = AccessLevel.PRIVATE)
+        @Getter
+        public static class Response {
+             int state;
+             Object error;
+             Result result;
+        }
 
-        return null; // todo
+        @FieldDefaults(level = AccessLevel.PRIVATE)
+        @Getter
+        public static class Result {
+             Map<String, Day> days;
+        }
+
+        @FieldDefaults(level = AccessLevel.PRIVATE)
+        @Getter
+        public static class Day {
+             String name;
+             String title;
+             List<Item> items;
+        }
+
+        @FieldDefaults(level = AccessLevel.PRIVATE)
+        @Getter
+        public static class Item {
+             String name;
+             boolean individual_exists;
+             String grp;
+
+             Map<Integer, Homework> homework;
+             Files files;
+        }
+
+        @FieldDefaults(level = AccessLevel.PRIVATE)
+        @Getter
+        public static class Homework {
+             int id;
+             String value;
+        }
+
+        @FieldDefaults(level = AccessLevel.PRIVATE)
+        @Getter
+        public static class Files {
+            public List<File> file;
+        }
+
+        @FieldDefaults(level = AccessLevel.PRIVATE)
+        @Getter
+        public static class File {
+            public int toid;
+            public String filename;
+            public String link;
+        }
+    }
+
+    public static class Authorization {
+        public static class TokenInfo {
+            public String token;
+            public String expires;
+
+            public String errorCode;
+            public String errorText;
+        }
+        public static class Response {
+            public int state;
+            public Object error;
+            @SerializedName("result")
+            public TokenInfo tokenInfo;
+        }
+        public static class Root {
+            public Response response;
+        }
     }
 }
